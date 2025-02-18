fcb55e5c14d2c26e5b54dde6bbfc66f2b7659329
Michael Short <michael.short094@gmail.com>
2025-02-18 16:05:06 -0700

    basic task code

--- cli/task.py
+++ cli/task.py
@@ -1,18 +1,48 @@
-# import stuff here (datetime?)
+from datetime import date, time
 
 class Task:
-    def __init__(self, title):
+    def __init__(self, title: str, date: date, time: time, desc: str, cat: str):
         """Constructs a new task object with the following data-
         self.title: str
-        self.date: ?
-        self.time: ?
+        self.date: date
+        self.time: time
         self.description: str
         self.category: str
         """
-        pass
+        self.title = title
+        self.date = date
+        self.time = time
+        self.description = desc
+        self.category = cat
 
-    def edit_task (self, arg1, arg2, arg_etc):
+    def edit_task(self, to_edit: str, input):
         """Edits a task's members based on the incoming data in the arguments.
         Knows what to edit and not to edit.
         Type checks and returns an error message if failed."""
-        pass
+        
+        # Type checking
+        match to_edit:
+            case "date":
+                if not isinstance(input, date):
+                    return f"Error: input ({input}) does not match to_edit ({to_edit})"
+            case "time":
+                if not isinstance(input, time):
+                    return f"Error: input ({input}) does not match to_edit ({to_edit})"
+            case _:
+                if not isinstance(input, str):
+                    return f"Error: input ({input}) does not match to_edit ({to_edit})"
+        
+        # Edit the attribute
+        match to_edit:
+            case "title":
+                self.title = input
+            case "date":
+                self.date = input
+            case "time":
+                self.time = input
+            case "description":
+                self.description = input
+            case "category":
+                self.category = input
+            case _:
+                return "Error editing case: Unknown Error"
