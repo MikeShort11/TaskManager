e773ccdaf56cc7347bcdd32da0d96342b7d2b21e
Michael Short <michael.short094@gmail.com>
2025-02-18 15:53:58 -0700

    basic task code

--- cli/task.py
+++ cli/task.py
@@ -1,7 +1,7 @@
-# import stuff here (datetime?)
+from datetime import date, time
 
 class Task:
-    def __init__(self, title):
+    def __init__(self, title:str, date:date, time:time, desc:str, cat:str):
         """Constructs a new task object with the following data-
         self.title: str
         self.date: ?
@@ -9,10 +9,43 @@ class Task:
         self.description: str
         self.category: str
         """
-        pass
 
-    def edit_task (self, arg1, arg2, arg_etc):
+        self.title = title
+        self.date = date
+        self.time = time
+        self.description = desc
+        self.category = cat
+
+
+    def edit_task (self, to_edit:str, input):
         """Edits a task's members based on the incoming data in the arguments.
         Knows what to edit and not to edit.
         Type checks and returns an error message if failed."""
-        pass
+
+        """check the types of to_edit and input"""
+        match to_edit:
+            case "date":
+                if type(input) != date:
+                    return f"Error: input ({input}) does not match to_edit ({to_edit})"
+            case "time":
+                if type(input) != time:
+                    return f"Error: input ({input}) does not match to_edit ({to_edit})"
+            case _:
+                if type(input) != str:
+                    return f"Error: input ({input}) does not match to_edit ({to_edit})"
+
+        """edit the attribute that is to be edited"""
+        match to_edit:
+            case "title":
+                self.title = input
+            case "date":
+                self.date = input
+            case "time":
+                self.time = input
+            case "description":
+                self.description = input
+            case "category":
+                self.category = input
+            case _:
+                return "Error editing case: Unknown Error"
+
