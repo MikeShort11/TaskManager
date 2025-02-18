454214be3e7976be4291bbad7c00dbae80d7b3a0
SecretMNC <pettkevinj@gmail.com>
2025-02-17 18:53:28 -0700

    changed name of test_add

--- cli/test_add.py
+++ cli/test_add.py
@@ -2,7 +2,7 @@ import task_list
 
 test_list = task_list.TaskList()
 
-def test_add():
+def test_add_task():
 
     test_list.add_task("Test_name")
     assert print(test_list["Test_name"]) == "Test_name"
