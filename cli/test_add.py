17d36e8eb3c4aae5b7c9b9a89c1b217438414d25
SecretMNC <pettkevinj@gmail.com>
2025-02-17 18:57:20 -0700

    fixed a bug in test_add pytest

--- cli/test_add.py
+++ cli/test_add.py
@@ -5,4 +5,4 @@ test_list = task_list.TaskList()
 def test_add_task():
 
     test_list.add_task("Test_name")
-    assert print(test_list["Test_name"]) == "Test_name"
+    assert print(test_list.list["Test_name"]) == "Test_name"
