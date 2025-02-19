9a098ddd545760a0a4a9332ca389102e35966476
MGoobs <g.turtle@icloud.com>
2025-02-19 12:14:40 -0700

    Started writing main

---
+++ cli/main.py
@@ -0,0 +1,20 @@
+import task_list
+import task
+from datetime import datetime
+
+def welcome_menu():
+    print("Enter a command: \n"
+          "1. Load Task Manager \n"
+          "2. Create Task Manager \n"
+          )
+
+def view_tasks():
+
+def task_manager_menu():
+    print("")
+
+
+def main(*args, **kwargs):
+    while True:
+        break
+
